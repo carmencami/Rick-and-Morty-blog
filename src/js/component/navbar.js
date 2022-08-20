cc48e2ad89ed10ef6edceeb3d21250531fcac4c0
carmencami <carmengreta3@outlook.es>
2022-08-20 01:26:03 +0000

    make-navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 	return (
 		<nav class="navbar bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand">RickandMorty</a>
+  <img src="https://s.ecrater.com/stores/442768/5e6e23856b19d_442768b.jpg" alt="logo" height="80px" width="90px"/>
     <form class="d-flex" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
       <button class="btn btn-outline-success" type="submit">Search</button>
